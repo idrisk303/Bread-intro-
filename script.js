a47974d3264dda9b73deddb1a76a3b8b33445a32
Idrisk <adelekeidrisayomide@gmail.com>
2026-07-28 03:46:51 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,33 @@
-console.log("Welcome to $BREAD 🚀");
+const messages=[
+"A NEW LEGEND IS RISING",
+"THE FUTURE IS BAKING",
+"JOIN THE BAKERS"
+];
+
+let index=0;
+
+const title=document.querySelector("h1");
+
+function nextScene(){
+
+title.style.opacity=0;
+
+setTimeout(()=>{
+
+title.innerHTML=messages[index];
+
+title.style.opacity=1;
+
+index++;
+
+if(index<messages.length){
+
+setTimeout(nextScene,3000);
+
+}
+
+},1000);
+
+}
+
+setTimeout(nextScene,2000);
